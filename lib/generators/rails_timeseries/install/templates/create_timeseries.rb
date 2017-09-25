7b5481d699c64801313446aff649a39f3a0de36e
JonghunYu <yuzong@naver.com>
2017-09-25 16:16:54 +0900

    added index to table

--- lib/generators/rails_timeseries/install/templates/create_timeseries.rb
+++ lib/generators/rails_timeseries/install/templates/create_timeseries.rb
@@ -1,8 +1,8 @@
 class CreateTimeseries < ActiveRecord::Migration<%="[#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}]"%>
   def change
     create_table :timeseries do |t|
-      t.date      "date", null: false
-      t.string    "scope", null: false
+      t.date      "date", null: false, index: true
+      t.string    "scope", null: false, index: true
       t.integer   "count", null: false
       t.timestamps null: false
     end
