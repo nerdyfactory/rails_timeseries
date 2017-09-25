9efd5073bcb1df0aece20faa67ebf2056338b151
JonghunYu <yuzong@naver.com>
2017-09-25 18:32:40 +0900

    changed ActiveRecord::Base to ApplicationRecord

--- lib/generators/rails_timeseries/install/templates/timeseries.rb
+++ lib/generators/rails_timeseries/install/templates/timeseries.rb
@@ -1,6 +1,6 @@
-class Timeseries < ActiveRecord::Base
+class Timeseries < ApplicationRecord
   def self.generate
-    ActiveRecord::Base.subclasses.each do |model|
+    ApplicationRecord.subclasses.each do |model|
       next unless model.respond_to?(:timeseries_scopes)
       model.send(:timeseries_scopes).each do |scope|
         timeseries = Timeseries.find_or_initialize_by(date: Time.now.to_date, scope: scope.to_s)
